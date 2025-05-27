805094263f8c122c0a07112efa3187a13744841c
Srikanth-235 <161904315+Srikanth-235@users.noreply.github.com>
2025-05-27 11:54:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
-
+	const firstname=document.getElementsByName("fname")[0].value.trim();
+	const lastname=document.getElementsByName("lname")[0].value.trim();
+	const concat1= `${firstname} ${lastname}`;
+	alert(concat1);
+     
 }
